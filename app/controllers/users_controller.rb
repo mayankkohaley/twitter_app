57b535d791afda0c615261832ca40b40aa013b31
Mayank Kohaley <mayank.kohaley@gmail.com>
2011-06-25 16:06:59 +0530

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   def new
   	@title = "Sign Up"
+  	@user = User.new
   end
   
   def show
   	@user = User.find(params[:id])
   	@title = @user.name
+  end
+  
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to the Twitter App"
+  		redirect_to @user
+  	else
+  		@title = "Sign Up"
+  		render 'new'
+  	end
   end
 
 end
