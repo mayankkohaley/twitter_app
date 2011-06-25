63ff83b517053b1f951872b65708e1126dcdd243
Mayank Kohaley <mayank.kohaley@gmail.com>
2011-06-25 13:39:25 +0530

    User model with passwords and user show page

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
 gem 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
 	gem 'rspec-rails', '2.6.1'
@@ -11,5 +12,6 @@ end
 group :test do
 	gem 'rspec-rails','2.6.1'
 	gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails', '1.0'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@
 #  updated_at :datetime
 #
 
+require "digest"
+
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	attr_accessor :password
@@ -23,5 +25,37 @@ class User < ActiveRecord::Base
 										:uniqueness => { :case_sensitive => false }
 	validates :password, :presence => true,
 											 :confirmation => true,
-											 :length => { :within => 6..40 }									
+											 :length => { :within => 6..40 }
+											 
+	before_save :encrypt_password
+	
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	
+	def self.authenticate(email,submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+	
+	private
+	
+	def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+	
+	def encrypt(string)
+		secure_hash("#{salt}--#{string}")
+	end
+	
+	def make_salt
+		secure_hash("#{Time.now.utc}--#{password}")
+	end
+	
+	def secure_hash(string)
+		Digest::SHA2.hexdigest(string)
+	end
+										
 end
