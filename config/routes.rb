d97f5fb11c45e967179364eee092220ce40644f1
Mayank Kohaley <mayank.kohaley@gmail.com>
2011-06-25 19:29:27 +0530

    Done with signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 TwitterApp::Application.routes.draw do
   resources :users
+	resources :sessions, :only => [:new, :create, :destroy ]
 
   match '/about',   :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
   match '/help',    :to => 'pages#help'
 	match '/signup',  :to => 'users#new'
+	match '/signin',  :to => 'sessions#new'
+	match '/signout', :to => 'sessions#destroy'
  
 	root :to => "pages#home" 
 end
